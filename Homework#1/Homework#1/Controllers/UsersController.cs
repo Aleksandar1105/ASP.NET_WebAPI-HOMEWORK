01b099d08f8308601005ec66ba27967c5de76c66
Aleksandar <109741787+Aleksandar1105@users.noreply.github.com>
2023-08-08 21:17:03 +0200

    Add files via upload

---
+++ Homework#1/Homework#1/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Homework_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetAll()
+        {
+            try
+            {
+                var users = StaticDb.UserNames;
+                return Ok(users);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing the request.");
+            }
+        }
+
+        [HttpGet("userId/{id}")]
+        public ActionResult<List<string>> GetById(int id)
+        {
+            try
+            {
+                if (id < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "The index is a negative number.");
+                }
+
+                if (id >= StaticDb.UserNames.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"The note with index {id} was not found.");
+                }
+
+                return Ok(StaticDb.UserNames[(int)id]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Contact the support team");
+            }
+        }
+    }
+}
---
+++ Homework#1/Homework#1/StaticDb.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_1
+{
+    public static class StaticDb
+    {
+        public static List<string> UserNames = new List<string>
+        {
+            "john_doe",
+            "jane_smith",
+            "alexander123",
+            "emily_jones",
+            "michael_brown",
+            "sarah_wilson",
+            "david_rodriguez",
+            "lisa_miller",
+            "jason_carter",
+            "olivia_jackson"
+        };
+    }
+}
